83646b9dc831188295a6da38747705cfa5c22e59
Ferrowrafael <ferrowrafael@gmail.com>
2020-03-11 23:40:34 +0100

    Creando busqueda peliculas

---
+++ scripts/prueba.js
@@ -0,0 +1,78 @@
+// const axios = require('axios'); 
+
+document.getElementById("button").addEventListener("click", getMovies);
+
+function getMovies(){
+  let busquedaP= document.getElementById("busqueda").value;
+  let image="https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
+  let key= "f1cbc5636aa2f2d3b7c9f1c1ca7c91de";
+
+  axios
+  .get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${busquedaP}`)
+  .then(res=>{
+    let peliculas = res.data.results
+    console.log(busquedaP)
+    console.log(peliculas)
+    peliculas.forEach(pelicula=>  
+       {document.querySelector('.peliculas').innerHTML +=`
+      <h2>${pelicula.title}</h2>
+      <img src="${image}${pelicula.poster_path}"></img>
+      <p>${pelicula.genre_ids}</p>
+      <p>${pelicula.overview}</p>
+      <button type="button" onclick="getDetails">Detalles</button>`
+    })
+  })
+  
+  
+  // .catch((err) => {console.log(err)})
+  
+}
+
+
+/* *** Genero *** */
+document.getElementById("buttonGen").addEventListener("click", getGenres);
+function getGenres(){
+
+  let image="https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
+  let key= "f1cbc5636aa2f2d3b7c9f1c1ca7c91de";
+
+  axios
+  .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=f1cbc5636aa2f2d3b7c9f1c1ca7c91de&language=en-US`)
+  .then(res=>{
+    let generos = res.data.results
+    console.log(generos)
+    generos.forEach(pelicula=>  
+      {document.querySelector('.generos').innerHTML +=`
+      <p>${pelicula.genre_ids}</p>
+      <p>${pelicula.overview}</p>
+      <button type="button" onclick="getDetails">Detalles</button>`
+    })
+  })
+  
+  
+  // .catch((err) => {console.log(err)})
+  
+}
+
+
+
+// function getDetails(){
+//   let key= "f1cbc5636aa2f2d3b7c9f1c1ca7c91de";
+
+//   axios
+//   .get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${pelicula.idP}`)
+//   .then(res=>{
+//     let peliculas = res.data.results
+//     console.log(busquedaP)
+//     console.log(peliculas)
+//     peliculas.forEach(pelicula=>  
+//        {document.querySelector('.peliculas').innerHTML +=`
+//       <h2>${pelicula.title}</h2>
+//       <img src="${image}${pelicula.poster_path}"></img>
+//       <p>${pelicula.genre_ids}</p>
+//       <p>${pelicula.overview}</p>
+
+//     })
+//   })
+  
+// }
