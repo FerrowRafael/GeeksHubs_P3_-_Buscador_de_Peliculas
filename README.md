d1897a1e62160fe85d72b29be7e7082b2afb4a58
Ferrowrafael <ferrowrafael@gmail.com>
2020-03-17 09:01:36 +0100

    Añadiendo README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# GeeksHubs_P3_-_Buscador_de_Peliculas
-Tercer proyecto Bootcamp GeeksHubs 
+# GeekFlix - Buscador_de_Peliculas
+
+<a href="https://ferrowrafael.github.io/Proyecto2-Diet_Me/"><img src="./images/DietMeLogo2.png" title="DietMeLogo" alt="DietMeLogo"></a>
+
+Página web dedicada a la busqueda de peliculas usando la API de TheMovieDB
+
+
+## Getting Started 🚀 
+
+[Prueba la Demo](https://ferrowrafael.github.io/GeeksHubs_P3_-_Buscador_de_Peliculas/) 
+
+O puedes clonarte el repositorio utilizando git clone (direccion del repositorio)
+
+
+## Built With 🛠️    
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* [TheMovieDB API](https://developers.themoviedb.org/4/getting-started/authorization)
+
+
+## Author 👨🏼‍💻
+
+* **Rafael Fernández Gómez** - [FerrowRafael](https://github.com/FerrowRafael)
+
+
+## Preview
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -2,72 +2,46 @@
 
 document.getElementById("button").addEventListener("click", getMovies);
 
-function getMovies(){
+
+function getMovies() {
   event.preventDefault();
   let busquedaP= document.getElementById("busqueda").value;
   let image="https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
   let key= "f1cbc5636aa2f2d3b7c9f1c1ca7c91de";
-  console.log("Hola")                                                                                                                                                                                 
+  // console.log("Hola")                                                                                                                                                                                 
   axios
   .get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${busquedaP}`)
   .then(res=>{
     const peliculas = res.data.results
-    // console.log(busquedaP)
-    // console.log(peliculas)
     document.querySelector('.peliculas').innerHTML = '';
     for (let pelicula of peliculas){
+      let pasarId = pelicula.id;
+      console.log(pasarId)
       document.querySelector('.peliculas').innerHTML +=`
-
-      <div class='pelicula col-lg-3 my-3'>
-      <h2 class="img-fluid">${pelicula.title}</h2>
-      <img id="img1" src="${image}${pelicula.poster_path}"></img>
+      <div class='pelicula col-lg-3 col-sm-3 col-xs-4 col-12 my-3 d-flex align-items-center flex-column proyectoImg'>
+        <h2 >${pelicula.title}</h2>
+        <img class="image" id="img1" src="${image}${pelicula.poster_path}"></img>
+        <div class="middle">
+          <div class="text">
+            <p>Resumen: ${pelicula.overview}</p>
+            <p>Fecha de Estreno: ${pelicula.release_date}</p>
+            <p>Genero: ${pelicula.genre_ids}</p>
+            <p>Nota: ${pelicula.vote_average}</p> 
+            <p id="reparto"> Reparto:</p>
+          </div>
+        </div>
       </div>`
-      // console.log(`${pelicula.id} ${pelicula.title}`)
-      // let IdGenre = pelicula.genre_ids
-      // getGenres(IdGenre)
-      // console.log(IdGenre)
-      cambiarImg(pelicula.id) 
-      console.log("Hola")
     }
     
   })
   .catch((err) => {console.log(err)})  
 }
 
-//Ver donde añadir
-/*<p>Resumen: ${pelicula.overview}</p>
-<p>Fecha de Estreno: ${pelicula.release_date}</p>
-<p>Nota: ${pelicula.vote_average}</p>
-<button type="button" onclick="getDetails">Ver detalles</button>
-*/
-
 //Cambiamos imagen
 function cambiarImg(id) {
   let imagen = document.getElementById("img1");
   if (imagen.src === "https://image.tmdb.org/t/p/w300_and_h450_bestv2/null") {
-    console.log("hola")
+    // console.log("hola")
     imagen.src = 'https://noescinetodoloquereluce.com/wp-content/uploads/2019/09/joker-a-1.jpg';
   }
 }
-
-
-/* *** Genero *** */
-
-function getGenres(IdGenre){
-  console.log(`Generos: ${IdGenre[0]}`)
-  let key= "f1cbc5636aa2f2d3b7c9f1c1ca7c91de";
-  axios
-  .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`)
-  .then(res=>{
-    let generos = res;
-    let genero = Object.values(generos);
-    genero.forEach(element => {
-      document.querySelector('.generos').innerHTML +=`
-      <p>${element.name}</p>`
-    })
-  })
-}
-
-  
-// Sustituir imagen
-
